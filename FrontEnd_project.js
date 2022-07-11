f6cef5d42e202001f98140c2a5ae5961386af6d1
SaefWael <108944845+SaefWael@users.noreply.github.com>
2022-07-11 19:22:26 +0200

    update

--- FrontEnd_project.js
+++ FrontEnd_project.js
@@ -11,10 +11,29 @@ Vue.component ('cards', {
   `,
 });
 
-
 new Vue ({
     el: '#app',
     data: {
         message: 'welcome',
     }
 })
+
+Vue.component ('cardss', {
+    props:['title','bold','paragraph'],
+    template: `
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{ title }}</h5>
+            <p class="card-text"><span>{{ bold }}</span>{{ paragraph }}</p>
+        </div>
+  </div>`,
+});
+
+new Vue ({
+    el: '#appp',
+    data: {
+        message: 'welcome',
+    }
+})
+
+
